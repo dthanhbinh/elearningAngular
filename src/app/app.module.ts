22cb6841d832c7b2c0027634cd3311e2ab5ac12a
unknown <dtbinh59@gmail.com>
2021-04-19 21:31:16 +0700

    'angular_routing'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+//Cấu hình Routing
+import { Routes,RouterModule } from "@angular/router";
+import { LoginComponent } from './Modules/home/login/login.component';
+import { RegisterComponent } from './Modules/home/register/register.component';
+import { HomePageComponent } from './Modules/home/home-page/home-page.component';
+ const appRoutes:Routes = [
+   {path:'login',component:LoginComponent},
+   {path:'register',component:RegisterComponent},
+   {path:'home',component:HomePageComponent},
+   {path:'',component:HomePageComponent},
+ ]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    //Router module này sẽ điều cho thẻ <router-outlet> tại app-component
+    BrowserModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
